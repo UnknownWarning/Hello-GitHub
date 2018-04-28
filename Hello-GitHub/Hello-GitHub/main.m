8025838996cea5c0dd40debe2f14f47bb15c94a9
UnknownWarning <38637150+UnknownWarning@users.noreply.github.com>
2018-04-28 16:50:29 +0800

    Add files via upload

---
+++ Hello-GitHub/Hello-GitHub/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Hello-GitHub
+//
+//  Created by UnknownWarning
+//  Copyright © 2018 UnknownWarning. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Create a repository on the GitHub.
+        NSLog(@"Hello, GitHub.");
+    }
+    return 0;
+}
